f14f5a04a08e34c482bf6f938fed6fe562c258f4
Bara Banat <barabanat@Baras-MacBook-Pro.local>
2024-08-04 19:06:14 -0400

    Reorganize project structure and remove node_modules

--- hashPassword.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const plainPassword = 'superadminbara'; // Replace with your desired password
-
-bcrypt.hash(plainPassword, saltRounds, (err, hash) => {
-    if (err) throw err;
-    console.log(hash); // This will print the hashed password
-});
